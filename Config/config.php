77c81f1a3546265e9b87dc34f62d386d8ee8fc9a
NutriX-Uneatlantico <67063758+NutriX-Uneatlantico@users.noreply.github.com>
2020-07-20 10:22:36 +0200

    Update cofig file to rename Cognito region

--- Config/config.php
+++ Config/config.php
@@ -2,6 +2,6 @@
 
 return [
     'name' => 'CognitoGuard',
-    'region' => env('AWS_REGION', 'us-east-1'),
+    'region' => env('COGNITO_AWS_REGION', 'us-east-1'),
     'user_pool_id' => env('COGNITO_USER_POOL_ID', '')
 ];
